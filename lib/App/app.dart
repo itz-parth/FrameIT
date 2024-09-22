f476807d026946019b43ba90dcbe9324b2f93ee6
Parth <parthapm2018@gmail.com>
2024-09-22 11:17:09 +0530

    Initial Commit

--- lib/App/app.dart
+++ lib/App/app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Demo App",
-      home: HomeView()
+      home:  HomeView()
     );
   }
 }
