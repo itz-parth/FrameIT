afb84a64c75cf3a86e9c8371657a9854ea8648a2
Parth <parthapm2018@gmail.com>
2024-09-27 20:03:35 +0530

    added search view

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -12,6 +12,7 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   List<dynamic> data = [];
+  bool isLoading = true; // Track loading state
 
   @override
   void initState() {
@@ -19,111 +20,167 @@ class _HomeViewState extends State<HomeView> {
     fetchData();
   }
 
-  void fetchData() async {
+  Future<void> fetchData() async {
     debugPrint("Fetching data...");
     const url = 'https://pixabay.com/api/?key=20212026-534ffc9d2c7c7400c6364783a&image_type=photo';
     final uri = Uri.parse(url);
-    final response = await http.get(uri);
 
-    if (response.statusCode == 200) {
-      final body = response.body;
-      final json = jsonDecode(body);
+    try {
+      final response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body);
+        setState(() {
+          data = json['hits'];
+          isLoading = false; // Set loading to false after data is fetched
+        });
+        debugPrint("Fetch complete");
+      } else {
+        setState(() {
+          isLoading = false; // Stop loading even if the fetch fails
+        });
+        debugPrint("Failed to fetch data: ${response.statusCode}");
+      }
+    } catch (e) {
       setState(() {
-        data = json['hits']; 
+        isLoading = false;
       });
-      debugPrint("Fetch complete");
-    } else {
-      debugPrint("Failed to fetch data: ${response.statusCode}");
+      debugPrint("Error fetching data: $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    return _home_view();
+  }
+
+  Scaffold _home_view() {
     return Scaffold(
-      appBar: App_Bar(),
-      body: body_data(),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: const Color.fromARGB(0, 0, 0, 0),
-        color: Colors.blue,
-        height: 50,
-        items: const [
-        Icon(
-          Icons.home
-        ),
-        Icon(
-          Icons.search
-        ),
-        Icon(
-          Icons.person
-        ),
-        
-      ]),
-    );
-  }
-
-
-  Column body_data() {
-    return Column(
-      children: [
-        const Divider(
-            height: 1,
-            color: Color.fromARGB(220, 0, 0, 0),
-            indent: 15,
-            endIndent: 100,
-          ),
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GridView.count(
-              crossAxisCount: 2,
-              children: List.generate(data.length, (index) {
-                String imgUrl = data[index]['webformatURL'];
-                return Padding(
-                  padding: const EdgeInsets.all(6),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      imgUrl,
-                      fit: BoxFit.cover,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  AppBar App_Bar() {
-    return AppBar(
-      // backgroundColor: Colors.blue,
-      toolbarHeight: 90,
-      title: const Padding(
-        padding: EdgeInsets.only(left: 5),
-        child: Column(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'FrameIt',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.w600,
+                'FrameIt',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold
+                ),
               ),
-            ),
-            SizedBox(height: 2),
             Text(
-              'Frame The World Through Lens',
+              'Frame The World Of Lens',
               style: TextStyle(
-                fontSize: 15,
-                color: Color.fromARGB(150, 0, 0, 0),
+                fontSize: 13,
+                color: Color.fromARGB(135, 0, 0, 0),
               ),
-            ),
+            )    
           ],
         ),
       ),
+      body: Column(
+        children: [
+          Divider(
+            thickness: 3,
+            height: 2,
+            indent: 15,
+            endIndent: 15,
+          ),
+          _buildImageGrid(),
+        ],
+      ),
+      bottomNavigationBar: _buildNavbar(),
+    );
+  }
+
+  Scaffold _search_view() {
+    return Scaffold(
+    appBar: AppBar(
+      toolbarHeight: 40,
+      title: Center(
+        child: Text(
+          'SearchIT',
+          style: TextStyle(
+            fontSize: 20
+          ),
+          )
+        ),
+    ),
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Divider(
+          height: 2,
+          thickness: 3,
+          indent: 15,
+          endIndent: 15,
+        ),
+        _buildSearchField(),
+        isLoading ? _buildLoadingIndicator() : _buildImageGrid(),
+      ],
+    ),
+    bottomNavigationBar: _buildNavbar(),
+  );
+  }
+
+  Widget _buildSearchField() {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(7),
+          ),
+          prefixIcon: Icon(Icons.search),
+          hintText: 'Search for photos...',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return Expanded(
+      child: Center(child: CircularProgressIndicator()),
+    );
+  }
+
+  Widget _buildImageGrid() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(data.length, (index) {
+            String imgUrl = data[index]['webformatURL'];
+            return Padding(
+              padding: const EdgeInsets.all(6),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                  imgUrl,
+                  fit: BoxFit.cover,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Center(child: Icon(Icons.error)); // Show error icon
+                  },
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNavbar() {
+    return CurvedNavigationBar(
+      backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+      color: Colors.blue,
+      height: 50,
+      items: const [
+        Icon(Icons.home),
+        Icon(Icons.search),
+        Icon(Icons.person),
+      ],
     );
   }
 }
